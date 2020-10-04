666de6a7571105a538e19dba1bee219c8b3e3d87
seckujdivad <juckes1david@gmail.com>
2020-10-04 18:49:35 +0100

    Added check for inserted calculator into makemove

--- makemove.py
+++ makemove.py
@@ -3,6 +3,9 @@ import shutil
 
 
 os.system("make")
-shutil.copyfile("fx-CG50TETRIS.g3a", "E:/fx-CG50TETRIS.g3a")
 
-print("Move was successful")
+if os.path.exists("E:"):
+    shutil.copyfile("fx-CG50TETRIS.g3a", "E:/fx-CG50TETRIS.g3a")
+    print("Move was successful")
+else:
+    print("Drive not inserted, move not attempted")
