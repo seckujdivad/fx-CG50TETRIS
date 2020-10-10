f0b5cec9400320cbff481e141c78c0d95f95519f
seckujdivad <juckes1david@gmail.com>
2020-10-10 01:42:58 +0100

    Added min and max methods

--- src/utilities.h
+++ src/utilities.h
@@ -13,3 +13,29 @@ void operator delete(void* pointer, size_t size);
 
 int RTC_rand(int ceil);
 int RTC_rand(int floor, int ceil);
+
+template<typename T>
+inline T max(T a, T b)
+{
+	if (a > b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+template<typename T>
+inline T min(T a, T b)
+{
+	if (a < b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
